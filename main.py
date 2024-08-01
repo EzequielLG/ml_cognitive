527ab5bbc11e8d26d8329c27a2498537dcf2f274
EzequielLG <A01734172@tec.mx>
2024-07-31 18:21:41 -0600

    changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from video_processing import gpt4_turbo_vision_video_model, video_indexing
+from video_processing import video_indexing, gpt4_turbo_vision_video_model
 import re
 
+# --------------------------------
+# Información del video a analizar
+# --------------------------------
 """
 - Debe ser un nombre único y sin espacios
 - Solo debe contener caracteres alfanuméricos y guiones medios
@@ -8,60 +11,25 @@ import re
 - Debe comenzar con un caracter alfanumérico
 - No debe terminar en guion medio
 """
-video_index_name = "test-videos-2"
+video_index_name = "test-video-index"
 """
-Video SAS (Shared Access Signature) URL (por ejemplo): 
+- Video SAS (Shared Access Signature) URL, por ejemplo: 
 https://<your-storage-account-name>.blob.core.windows.net/<your-container-name>/<your-video-name>?<SAS-token>
 """
 video_SAS_url = "https://gpt4vsamples.blob.core.windows.net/videos/Microsoft%20Copilot%20Short.mp4"
 """
-Debe ser único
+- El ID debe ser único
 """
-video_id = "test-video-1"
+video_id = "video-test"
 
-video_config = {
-    "video_index_name": video_index_name,
-    "video_SAS_url": video_SAS_url,
-    "video_id": video_id
-}
-
-# Creación de index (ejecutar una única vez por video)
-# video_indexing(video_config["video_index_name"], video_config["video_SAS_url"], video_config["video_id"])
-
-# Rol del modelo
-sys_message = "Eres un asistente en el análisis de videos."
-# Prompt para el modelo
+# --------------------------------
+# Configuraciones del modelo de IA
+# --------------------------------
+system_prompt = "Eres un asistente en el análisis de videos."
 user_prompt = "Describe todo lo que ves en este video:"
 
-# El contenido del tipo "acv_document_id" debe declararse en primer lugar en la lista del contenido del usuario, 
-# ya que, de lo contrario puede esperarse un comportamiento inesperado
-model_config = [
-    {
-        "role": "system", 
-        "content": [
-            {
-                "type": "text", 
-                "text": sys_message
-            }
-        ]
-    },
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "acv_document_id", 
-                "acv_document_id": video_config["video_id"]
-            }, 
-            {
-                "type": "text", 
-                "text": user_prompt
-            }
-        ]
-    }
-]
-
 try:
-    response = gpt4_turbo_vision_video_model(model_config, video_config)
+    response = gpt4_turbo_vision_video_model(system_prompt, user_prompt, video_index_name, video_SAS_url, video_id)
     text = response["choices"][0]["message"]["content"]
     sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text)
     for sentence in sentences:
--- video_processing.py
+++ video_processing.py
@@ -3,16 +3,21 @@ import requests
 import json
 import time
 
-def gpt4_turbo_vision_video_model(model_config, video_config):
+def gpt4_turbo_vision_video_model(system_prompt, user_prompt, video_index_name, video_SAS_url, video_id):
 
+    # Creación de index y adición de video al index
+    video_indexing(video_index_name, video_SAS_url, video_id)
+
+    # Análisis de video
     api_url = f"{openai_api_endpoint}/openai/deployments/{gpt_deployment_name}/extensions/chat/completions?api-version={openai_api_version}"
-    
     headers = {
         "Content-Type": "application/json",
         "api-key": openai_api_key,
         "x-ms-useragent": "Azure-GPT-4V-video/1.0.0"
     }
 
+    # El contenido del tipo "acv_document_id" debe declararse en primer lugar en la lista del contenido del usuario, 
+    # ya que, de lo contrario puede esperarse un comportamiento inesperado
     payload = {
         "model": "gpt-4-vision-preview",
         "dataSources": [
@@ -21,9 +26,9 @@ def gpt4_turbo_vision_video_model(model_config, video_config):
                 "parameters": {
                     "computerVisionBaseUrl": f"{vision_api_endpoint}/computervision",
                     "computerVisionApiKey": vision_api_key,
-                    "indexName": video_config["video_index_name"],
+                    "indexName": video_index_name,
                     "videoUrls": [
-                        video_config["video_SAS_url"]
+                        video_SAS_url
                     ]
                 }
             }
@@ -33,14 +38,37 @@ def gpt4_turbo_vision_video_model(model_config, video_config):
                 "enabled": True
             }
         },
-        "messages": model_config,
+        "messages": [
+            {
+                "role": "system", 
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": system_prompt
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "acv_document_id", 
+                        "acv_document_id": video_id
+                    }, 
+                    {
+                        "type": "text", 
+                        "text": user_prompt
+                    }
+                ]
+            }
+        ],
         "temperature": 0.7,
         "top_p": 0.95,
         "max_tokens": 800
     }
 
     try:
-        response = requests.post(api_url, headers = headers, json = payload, timeout = 30)
+        response = requests.post(api_url, headers = headers, json = payload, timeout = 100)
         response.raise_for_status() # Lanza una excepción si se obtiene un código de error HTTP
         return response.json()
     except requests.RequestException as e:
@@ -86,7 +114,7 @@ def add_video_to_index(video_index_name, video_SAS_url, video_id):
     }
     return requests.put(url, headers = headers, data = json.dumps(data))
 
-def wait_for_video_adding_process(video_index_name, max_retries = 30):
+def wait_for_video_adding_process(video_index_name, max_retries = 100):
     url = f"{vision_api_endpoint}/computervision/retrieval/indexes/{video_index_name}/ingestions?api-version=2023-05-01-preview"
     headers = {
         "Ocp-Apim-Subscription-Key": vision_api_key
@@ -98,12 +126,8 @@ def wait_for_video_adding_process(video_index_name, max_retries = 30):
         if response.status_code == 200:
             state_data = response.json()
             if state_data["value"][0]["state"] == "Completed":
-                print(state_data)
-                print("Procesamiento de video desde storage completado.")
                 return True
             if state_data["value"][0]["state"] == "Failed":
-                print(state_data)
-                print("Procesamiento de video desde storage fallido.")
                 return False
         retries += 1
     return False
@@ -111,12 +135,14 @@ def wait_for_video_adding_process(video_index_name, max_retries = 30):
 def video_indexing(video_index_name, video_SAS_url, video_id):
     # Creación de index
     response = create_video_index(video_index_name)
-    print(response.status_code, response.text)
+    
+    # Si el index del video ya existe entonces ya no se procesa
+    if(response.status_code == 409):
+        return
 
     # Adición de video al index creado previamente
     response = add_video_to_index(video_index_name, video_SAS_url, video_id)
-    print(response.status_code, response.text)
-
+    
     # Finalización del procesamiento de video
     if not wait_for_video_adding_process(video_index_name):
         print("Procesamiento de video no completado dentro del tiempo esperado.")
